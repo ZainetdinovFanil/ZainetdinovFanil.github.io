564d55ef442bbd162c1bd5f2bcfd2a02a422e26e
ZainetdinovFanil <90759577+ZainetdinovFanil@users.noreply.github.com>
2022-05-30 15:42:39 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -244,7 +244,6 @@ let sliderArrowRight = document.querySelector('.slider-arrow-right');
 let sliderBlocks = document.querySelectorAll('.slider-block');
 let sliderInner = document.querySelector('.slider-inner');
 
-let sliderBlocksWidth = sliderBlocks[2].offsetWidth + 'px';
 
 
 sliderBlocks[0].style.width=0 + 'px';
@@ -255,30 +254,33 @@ console.log(sliderArrowsSvgs);
 sliderArrowRight.addEventListener('hover',function(){
   sliderArrowsSvgs[1].style.stroke='#FAFF00';
 });
+let sliderBlocksWidth = 0;
+if(header.offsetWidth>1200){
+  sliderBlocksWidth = 380 + 'px';}
+else if(header.offsetWidth>1024&& header.offsetWidth<1201){
+  sliderBlocksWidth = 300 + 'px';}
+else if(header.offsetWidth>768&& header.offsetWidth<1025){
+  sliderBlocksWidth = 200 + 'px';}
+else if(header.offsetWidth>600&& header.offsetWidth<769){
+  sliderBlocksWidth = 160 + 'px';}
+else if(header.offsetWidth>480&& header.offsetWidth<601){
+  sliderBlocksWidth = 120 + 'px';}
+else if(header.offsetWidth<481){
+  sliderBlocksWidth = 30 + '%';};
 
-
-
-  sliderArrowRight.addEventListener('click', function (){
+console.log(sliderBlocksWidth);
+    sliderArrowRight.addEventListener('click', function (){
     sliderBlocks = document.querySelectorAll('.slider-block');
-
     sliderBlocks[0].style.width=sliderBlocksWidth;
     sliderBlocks[3].style.width=0 + 'px';
     sliderBlocks[1].classList.add('slider-block-mid');
     sliderBlocks[2].classList.remove('slider-block-mid');
     sliderBlocks[4].innerHTML=sliderBlocks[3].innerHTML;
-
-
     setTimeout(function(){
-      sliderBlocks[4].id="last_slide";
-      sliderBlocks[0].id="first_slide";
-
       let clone = sliderBlocks[4].cloneNode(true);
       sliderBlocks[4].remove();
-
       sliderBlocks = document.querySelectorAll('.slider-block');
       sliderInner.insertBefore(clone, sliderBlocks[0]);
-      sliderBlocks[0].id="w";
-      sliderBlocks[1].id="w";
   }, 10);
 
 /*   sliderArrowRight.removeEventListener('click',sliderArrowLeftFunction()); */
@@ -286,33 +288,115 @@ sliderArrowRight.addEventListener('hover',function(){
 
   sliderArrowLeft.addEventListener('click',function /* sliderArrowLeftFunction */(){
     sliderBlocks = document.querySelectorAll('.slider-block');
-
     sliderBlocks[4].style.width=sliderBlocksWidth;
     sliderBlocks[1].style.width=0 + 'px';
     sliderBlocks[3].classList.add('slider-block-mid');
     sliderBlocks[2].classList.remove('slider-block-mid');
     sliderBlocks[0].innerHTML=sliderBlocks[1].innerHTML;
-
     setTimeout(function(){
-      sliderBlocks[0].id="first_slide";
-      sliderBlocks[4].id="last_slide";
-
       let clone = sliderBlocks[0].cloneNode(true);
-      sliderInner.append(clone);
       sliderBlocks[0].remove();
-
       sliderInner.appendChild(clone); 
       sliderBlocks = document.querySelectorAll('.slider-block');
-      sliderBlocks[4].id="w";
-      sliderBlocks[3].id="w";
     }, 10);
 
   });
 
-  function get(){
-    sliderInner = document.querySelector('.slider-inner');
-    var div = document.createElement('div');
-    sliderInner.appendChild(div); 
-    console.log(div);
 
-  }
+
+
+
+//смена картинок где лицензия
+let docsRightImg = document.querySelector('.docs_right-img');
+let docsLeftBtn = document.querySelectorAll('.docs_left-btn');
+
+docsLeftBtn[0].addEventListener('click',function(){
+  docsRightImg.src='https://eduexp.ru/wp-content/uploads/2021/09/lic_center.png'
+});
+docsLeftBtn[1].addEventListener('click',function(){
+  docsRightImg.src='https://eduexp.ru/wp-content/uploads/2021/09/diplom-slider-1.png'
+});
+
+docsLeftBtn[2].addEventListener('click',function(){
+  docsRightImg.src='https://eduexp.ru/wp-content/uploads/2021/09/diplom-slider-2.png'
+});
+
+
+
+
+
+
+
+//слайдер дипломов
+
+  
+let diplomArrowLeft = document.querySelector('.diplom-arrow-left');
+let diplomArrowRight = document.querySelector('.diplom-arrow-right');
+let diplomBlocks = document.querySelectorAll('.diplom-block');
+let diplomInner = document.querySelector('.diplom-inner');
+
+
+
+diplomBlocks[0].style.width=0 + 'px';
+diplomBlocks[4].style.width=0 + 'px';
+
+let diplomArrowsSvgs = document.querySelectorAll('.diplom-arrows-svg path');
+
+diplomArrowRight.addEventListener('hover',function(){
+  diplomArrowsSvgs[1].style.stroke='#FAFF00';
+});
+
+
+
+diplomArrowRight.addEventListener('click', function (){
+  let diplomBlocksWidth = diplomBlocks[2].offsetWidth + 'px';
+    diplomBlocks = document.querySelectorAll('.diplom-block');
+
+    diplomBlocks[0].style.width=diplomBlocksWidth;
+    diplomBlocks[3].style.width=0 + 'px';
+    diplomBlocks[1].classList.add('diplom-block-mid');
+    diplomBlocks[2].classList.remove('diplom-block-mid');
+    diplomBlocks[4].innerHTML=diplomBlocks[3].innerHTML;
+
+
+    setTimeout(function(){
+      diplomBlocks[4].id="last_slide";
+      diplomBlocks[0].id="first_slide";
+
+      let clone = diplomBlocks[4].cloneNode(true);
+      diplomBlocks[4].remove();
+
+      diplomBlocks = document.querySelectorAll('.diplom-block');
+      diplomInner.insertBefore(clone, diplomBlocks[0]);
+      diplomBlocks[0].id="w";
+      diplomBlocks[1].id="w";
+  }, 10);
+
+/*   sliderArrowRight.removeEventListener('click',sliderArrowLeftFunction()); */
+  });
+
+  diplomArrowLeft.addEventListener('click',function /* sliderArrowLeftFunction */(){
+    let diplomBlocksWidth = diplomBlocks[2].offsetWidth + 'px';
+    diplomBlocks = document.querySelectorAll('.diplom-block');
+
+    diplomBlocks[4].style.width=diplomBlocksWidth;
+    diplomBlocks[1].style.width=0 + 'px';
+    diplomBlocks[3].classList.add('diplom-block-mid');
+    diplomBlocks[2].classList.remove('diplom-block-mid');
+    diplomBlocks[0].innerHTML=diplomBlocks[1].innerHTML;
+
+    setTimeout(function(){
+      diplomBlocks[0].id="first_slide";
+      diplomBlocks[4].id="last_slide";
+
+      let clone = diplomBlocks[0].cloneNode(true);
+      diplomInner.append(clone);
+      diplomBlocks[0].remove();
+
+      diplomInner.appendChild(clone); 
+      diplomBlocks = document.querySelectorAll('.diplom-block');
+      diplomBlocks[4].id="w";
+      diplomBlocks[3].id="w";
+    }, 10);
+
+  });
