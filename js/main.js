72721cbd8254dc373949d9f3862a848d5efe9f42
ZainetdinovFanil <90759577+ZainetdinovFanil@users.noreply.github.com>
2022-06-20 22:03:43 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -755,3 +755,41 @@ diplomArrowRight.addEventListener('click', function (){
     }, 10);
 
   });
+
+
+
+
+
+  //анимация при скролле
+  const animItems = document.querySelectorAll('._anim-items');
+  //добавляем блоку класс _anim-items, настраиваем смещение до и после добавления класса _active
+  if(animItems.length>0){
+    window.addEventListener('scroll',animOnScroll)
+    function animOnScroll(animParams){
+      for(let index = 0; index<animItems.length; index++){
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight/animStart;
+        if(animItemHeight> window.innerHeight){
+            animItemPoint = window.innerHeight - window.animItemHeight/animStart;
+        }
+
+        if((pageYOffset>animItemOffset - animItemPoint) && pageYOffset<(animItemOffset+animItemHeight)){
+          animItem.classList.add('_active');
+        }else{
+          animItem.classList.remove('_active');
+        }
+      }
+    }
+    function offset(el){
+      const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return{ top: rect.top + scrollTop,left: rect.left + scrollLeft}
+    }
+    animOnScroll();
+  }
+
